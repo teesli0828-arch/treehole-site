6fb3a57e63d3eefbbda7c0ef6a699306b4547385
teesli0828-arch <teesli0828@gmail.com>
2026-04-27 16:28:49 +0800

    Update supabase.js

--- pages/lib/supabase.js
+++ pages/lib/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_NEXT_PUBLIC_SUPABASE_URL=https://btedlycwtmtpydnujfuo.supabase.co,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_sb_publishable_htKY8nSFKAJFplSNovWWpg_bwOZnPoG
 )
 
 export default supabase
